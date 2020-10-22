8295317777efb50a4f3dd58df32273d9cffbd8ff
KremerMichael <kremeremichael@gmail.com>
2020-10-22 15:32:45 -0400

    auto push run at Thu 22 Oct 2020 03:32:45 PM EDT

--- survey.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-From email this takes you to login
-http://patientconnect.bu.edu/
-n homepage this takes you to survey
-https://patientconnect.bu.edu/Mvc/Patients/QuarantineSurvey
-on survey this proceeds
-https://patientconnect.bu.edu/CheckIn/Survey/ShowAll/21
